bf5bbf9e2499110207687e4d4bf53927ca3fcb72
Lautaro Reche <lautaroreche1@gmail.com>
2025-02-25 08:05:42 +0100

    Add background

--- disco_pizza_web_page_app/admin.py
+++ disco_pizza_web_page_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.sessions.models import Session
+from disco_pizza_web_page_app.models import Product
+    
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price')
+    search_fields = ('id', 'name', 'price')
 
-# Register your models here.
+
+admin.site.register(Session)
+admin.site.register(Product, ProductAdmin)
